ce72f58635af3f8097cc8e5859a7d10d24b1d885
strigenzc <97646601+strigenzc@users.noreply.github.com>
2022-03-18 10:52:34 +1100

    JavaScript Updates
    -Added Current date and itme of loast weather update
    -Added Humidity and WindSpeed Accurate adte

--- src/index.js
+++ src/index.js
@@ -1,4 +1,24 @@
+function formatDate(timestamp) {
+    // calculates the date for last update
+    let date = new Date (timestamp);
+    let hours = date.getHours();
+    if (hours <10) {
+        hours = `0${hours}`;
+    }
+    let minutes = date.getMinutes ();
+    if (minutes <10){
+        minutes = `0${minutes}`;
+    }
+
+    let dayList = date.getDay();
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let day = days[date.getDay()];
+
+    return `Last updated ${day} ${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
+    //changing the inner HTML to match current weather data
 let temperatureElement= document.querySelector("#temperature");
 let cityElement= document.querySelector("#city");
 let dateElement= document.querySelector("#currentDate");
@@ -7,7 +27,7 @@ let windElement= document.querySelector("#wind-speed");
 let humidityElement= document.querySelector("#humidity")
 temperatureElement.innerHTML = Math.round(response.data.main.temp);
 cityElement.innerHTML = response.data.name;
-dateElement.innerHTML = "17th of March";
+dateElement.innerHTML = formatDate(response.data.dt * 1000);
 descriptionElement.innerHTML = response.data.weather[0].description;
 windElement.innerHTML = Math.round(response.data.wind.speed);
 humidityElement.innerHTML = Math.round(response.data.main.humidity);
@@ -22,6 +42,9 @@ let apiURL =
 
   console.log(apiURL);
   axios.get(apiURL).then(displayTemperature);
+  // ^ adds in axios so you can connect with Open weather for data
+
+
 
 
 
