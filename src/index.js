eb2bddeae0dd4e90dfd6cc129a5f6c6f3894c792
strigenzc <97646601+strigenzc@users.noreply.github.com>
2022-03-19 16:51:10 +1100

    Changed Main Icon to match weather

--- src/index.js
+++ src/index.js
@@ -1,51 +1,61 @@
 function formatDate(timestamp) {
-    // calculates the date for last update
-    let date = new Date (timestamp);
-    let hours = date.getHours();
-    if (hours <10) {
-        hours = `0${hours}`;
-    }
-    let minutes = date.getMinutes ();
-    if (minutes <10){
-        minutes = `0${minutes}`;
-    }
+  // calculates the date for last update
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
 
-    let dayList = date.getDay();
-    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    let day = days[date.getDay()];
+  let dayList = date.getDay();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
 
-    return `Last updated ${day} ${hours}:${minutes}`;
+  return `Last updated ${day} ${hours}:${minutes}`;
 }
 
 function displayTemperature(response) {
-    //changing the inner HTML to match current weather data
-let temperatureElement= document.querySelector("#temperature");
-let cityElement= document.querySelector("#city");
-let dateElement= document.querySelector("#currentDate");
-let descriptionElement= document.querySelector("#description");
-let windElement= document.querySelector("#wind-speed");
-let humidityElement= document.querySelector("#humidity")
-temperatureElement.innerHTML = Math.round(response.data.main.temp);
-cityElement.innerHTML = response.data.name;
-dateElement.innerHTML = formatDate(response.data.dt * 1000);
-descriptionElement.innerHTML = response.data.weather[0].description;
-windElement.innerHTML = Math.round(response.data.wind.speed);
-humidityElement.innerHTML = Math.round(response.data.main.humidity);
-console.log(response.data);
+  //changing the inner HTML to match current weather data
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let dateElement = document.querySelector("#currentDate");
+  let descriptionElement = document.querySelector("#description");
+  let windElement = document.querySelector("#wind-speed");
+  let humidityElement = document.querySelector("#humidity");
+  let iconElement = document.querySelector("#icon");
+
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  humidityElement.innerHTML = Math.round(response.data.main.humidity);
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute(
+      "alt", response.data.weather[0].description);
 }
 
 let apiKey = "7bcf0da6ca80b20c501d86d32cc003a7";
-let cityname = "Chicago";
+let cityname = "Paris";
 let units = "metric";
-let apiURL =
-  `https://api.openweathermap.org/data/2.5/weather?q=${cityname}&appid=${apiKey}&units=${units}`;
+let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityname}&appid=${apiKey}&units=${units}`;
 
-  console.log(apiURL);
-  axios.get(apiURL).then(displayTemperature);
-  // ^ adds in axios so you can connect with Open weather for data
+console.log(apiURL);
+axios.get(apiURL).then(displayTemperature);
+// ^ adds in axios so you can connect with Open weather for data
 
-
-
-
-
-
+//
